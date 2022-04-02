00ed7585a833e8b4f38ea2200e7bdf2a037b0f6e
折田幸夫 <3204704@gmail.com>
2022-04-02 14:58:04 +0900

    feat 1302

---
+++ All/1302.js
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=1302 lang=javascript
+ *
+ * [1302] Deepest Leaves Sum
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var deepestLeavesSum = function (root) {
+  let result = 0;
+  let maxLevel = -Infinity;
+
+  const dfs = (node, level) => {
+    if (node == null) return 0;
+
+    if (level === maxLevel) {
+      result += node.val;
+    } else if (level > maxLevel) {
+      maxLevel = level;
+      result = node.val;
+    }
+
+    dfs(node.left, level + 1);
+    dfs(node.right, level + 1);
+  };
+
+  dfs(root, 1);
+
+  return result;
+};
+// @lc code=end
